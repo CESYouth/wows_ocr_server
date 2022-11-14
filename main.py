6922fbb5e380675abf9bfb095c32bcaa34a7f3f3
youth <2485638793@@qq.com>
2022-11-15 00:30:39 +0800

    主方法

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ import json
 import uvicorn
 import shutil
 import text
+import hashlib
+import base64
+import cv2
+import get_Hash
 from paddleocr import PaddleOCR
 from distutils.util import strtobool
 
@@ -19,17 +23,41 @@ app = FastAPI()
 if not os.path.exists('./save'):
     os.madir('./save')
 
-@app.post('/OCR/')
+@app.post('/OCR/') #OCR服务
 def get_item_list(url:str = Form()):
-    start = time.time()#请求开始
+    start = time.time()
     file_name = path+'/image'+str(random.randint(0, 1000)) + '.jpg'
+    file_name_S = path+'/image'+str(random.randint(0, 1000)) + '_S.jpg'
     try:
         dowtime = time.time()
-        imgdow = image.img_dow(url,file_name)
+        imgdow = image.img_dow(url,file_name,file_name_S)
         dowtimend = time.time()
         if imgdow != '图像不符合':
             ocr_guolv = time.time()
-            result = ocr2.ocr(file_name, rec=False,cls=False)
+            
+            with open(path+'/data.json','r') as f:
+                jsonData = f.read()
+                f.close()
+            # print(jsonData)
+            Hash_list = json.loads(jsonData)
+            Hashmatching = False
+            img = cv2.imread(file_name_S,1)
+            Hash = get_Hash.pHash(img)
+            for key in Hash_list:
+                    data = Hash_list[key].replace('[','').replace(']','').replace(' ','').split(',')
+                    for i in range(len(data)):
+                        data[i] = int(data[i])
+                    # print(data)
+                    dst = get_Hash.cmpHash(Hash,data)
+                    if dst < 10:
+                        print(key,file_name_S,dst)
+                        Hashmatching = True
+                        break
+            # print(Hashmatching)
+            if not Hashmatching:
+                result = ocr2.ocr(file_name, rec=False,cls=False)
+            else:
+                result = '1'
             ocr_guolv_end = time.time()
             if len(result) < 12 and len(result) > 0:
                 result = ocr.ocr(file_name, cls=True)
@@ -37,9 +65,11 @@ def get_item_list(url:str = Form()):
             else:
                 string = '未匹配'
             if 'wws ' in string and strtobool(config['save_image']):
-                shutil.move(file_name,'./save/image'+str(len(os.listdir('./save/'))+1)+'.jpg')
-            else:
-                os.remove(file_name)
+                with open(file_name_S,'rb') as f:
+                    md = hashlib.md5()
+                    md.update(f.read())
+                    md5 = md.hexdigest()
+                shutil.move(file_name_S,'./save/'+md5+'.jpg')
         else:
             string = '未匹配'
         if strtobool(config['time_log']):
@@ -47,23 +77,46 @@ def get_item_list(url:str = Form()):
             if imgdow != '图像不符合':
                 print('图片过滤耗时：',ocr_guolv_end-ocr_guolv)
             print("总耗时：", time.time() - start,time.strftime('%Y-%m-%d-%Hh-%Mm-%Ss',time.localtime(time.time())))
-        
+        try:
+            os.remove(file_name)
+        except:
+            pass
+        try:
+            os.remove(file_name_S)
+        except:
+            pass
         print(string)
         return string
     except Exception as e:
         traceback.print_exc()
         try:
             os.remove(file_name)
-            return "未匹配"
         except:
-            return "未匹配"
             pass
+        try:
+            os.remove(file_name_S)
+        except:
+            pass
+        return "未匹配"
 
-# 运行程序
+
+@app.post('/ImageRandom/')#获取随机表情包
+def get_image():
+    global path
+    file_list = os.listdir(path+'/save/')
+    file_long = len(file_list)
+    with open(path+'/save/'+file_list[random.randint(0, file_long-1)],'rb') as f:
+        image = f.read()
+        f.close()
+    image_base64 = str(base64.b64encode(image), encoding='utf-8')
+    return image_base64
+    
+    
+    
 start = time.time()
 ocr2 = PaddleOCR(use_gpu=True,
                 det_model_dir='./inference/db_mv3_infer',
-                use_mp=True, total_process_num=6,show_log=False)
+                use_mp=True, total_process_num=6,show_log=False,drop_score=0.1)
 ocr = PaddleOCR(use_angle_cls=True, lang="ch", use_gpu=strtobool(config['gpu']),
                 rec_model_dir='./inference/ch_PP-OCRv3_rec_infer/',
                 cls_model_dir='./inference/ch_ppocr_mobile_v2.0_cls_infer/',
